75a8d8d94e524c4816073c6e3fdce8f4234245fb
Jura <juratitov@gmail.com>
2017-11-20 23:56:52 +0200

    Section 3. Lecture 14

--- app.js
+++ app.js
@@ -2,5 +2,25 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
+
+const argv = yargs.argv;
+var command = process.argv[2];
+
+console.log('Command: ', command);
+console.log('Yargs: ', argv);
+
+if(command === 'add'){
+    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {    
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title)
+} else if (command === 'remove') {
+    notes.removeNote(argv.title)
+} else {
+    console.log('Command not recognized');
+}
